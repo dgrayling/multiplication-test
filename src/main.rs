b4abffb7bc11d36d3cb2cc89320e97e75fab3b41
vimes <daniel.grayling.2020@gmail.com>
2022-05-08 16:17:46 +0100

    simplication

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,39 @@ pub mod equation {
     }
 }
 
+struct Puzzle {
+    s1: String,
+    s2: String,
+    n3: i32
+}
+
+fn generate_puzzle() -> Puzzle {
+    let n1 = equation::generate_number();
+    let n2 = equation::generate_number();
+    let s1 = n1.to_string();
+    let s2 = n2.to_string();
+    let n3 = n1*n2;
+    return Puzzle {
+        s1, s2, n3
+    }
+}
+
 fn main() -> () {
 
-    loop {        
-        // let number = equation::generate_equation();
-        let n1 = equation::generate_number();
-        let s1 = n1.to_string();
-        let n2 = equation::generate_number();
-        let s2 = n2.to_string();
-        println!("What is {} times {}", s1, s2);
+    loop {
+        let puzzle = generate_puzzle();
 
-        let n3 = n1*n2;
-        // let s3 = n3.to_string();
-        // println!("s3 {}", s3);
+        println!("What is {} times {}", puzzle.s1, puzzle.s2);
 
         let x = util::read();
 
         match x {
             Ok(x) => {
-                let l = x.parse::<String>().unwrap();
-                let m = l.parse::<i32>();
+                let m = x.parse::<String>().unwrap().parse::<i32>();
                 match m {
                     Ok(m) => {
                         println!("You entered the number {}", m);
-                        if m == n3 {
+                        if m == puzzle.n3 {
                             println!("correct");
                         } else {
                             println!("wrong");
